8221adc704d6d61f2d12a390bf151727c1d35b13
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2021-01-14 01:07:37 -0300

    Mapeando objetos com modelMapper

--- src/main/java/com/samuel/barbearia/mapper/ServicoMapper.java
+++ src/main/java/com/samuel/barbearia/mapper/ServicoMapper.java
@@ -5,11 +5,18 @@ import com.samuel.barbearia.requests.ServicoPostRequestBody;
 import com.samuel.barbearia.requests.ServicoPutRequestBody;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+import org.modelmapper.ModelMapper;
 
 @Mapper(componentModel = "spring")
-public abstract class ServicoMapper {
+public class ServicoMapper {
+
+    ModelMapper modelMapper = new ModelMapper();
 
     public static final ServicoMapper  INSTANCE = Mappers.getMapper( ServicoMapper .class );
-    public abstract  Servico toServico(ServicoPostRequestBody servicoPostRequestBody);
-    public abstract  Servico toServico1(ServicoPutRequestBody servicoPutRequestBody);
+    public   Servico toServico(ServicoPostRequestBody servicoPostRequestBody){
+        return  modelMapper.map(servicoPostRequestBody, Servico.class);
+    }
+    public  Servico toServico1(ServicoPutRequestBody servicoPutRequestBody){
+        return modelMapper.map(servicoPutRequestBody, Servico.class);
+    }
 }
