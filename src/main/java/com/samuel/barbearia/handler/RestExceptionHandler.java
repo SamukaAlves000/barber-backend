b14bf435c7be832d05fefc6a4ff17dd48ce76b0e
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2021-01-14 11:30:35 -0300

    Handler para validação de campos

--- src/main/java/com/samuel/barbearia/handler/RestExceptionHandler.java
+++ src/main/java/com/samuel/barbearia/handler/RestExceptionHandler.java
@@ -2,12 +2,17 @@ package com.samuel.barbearia.handler;
 
 import com.samuel.barbearia.exception.BadRequestException;
 import com.samuel.barbearia.exception.BadRequestExceptionDetails;
+import com.samuel.barbearia.exception.ValidationExceptionDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestExceptionHandler {
@@ -21,6 +26,27 @@ public class RestExceptionHandler {
                 .details(bre.getMessage())
                 .developerMessage(bre.getClass().getName())
                 .build(), HttpStatus.BAD_REQUEST);
+
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException (MethodArgumentNotValidException ex){
+
+
+        List<FieldError> fieldErrorList = ex.getBindingResult().getFieldErrors();
+        String fields = fieldErrorList.stream().map(FieldError::getField).collect(Collectors.joining(","));
+        String fieldsMessage = fieldErrorList.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request exception, Invalid Fields")
+                        .details("Check the field(s) error")
+                        .developerMessage(ex.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldsMessage)
+                        .build(), HttpStatus.BAD_REQUEST);
 
     }
 }
--- src/main/java/com/samuel/barbearia/requests/ServicoPostRequestBody.java
+++ src/main/java/com/samuel/barbearia/requests/ServicoPostRequestBody.java
@@ -2,13 +2,14 @@ package com.samuel.barbearia.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 
 @Data
 public class ServicoPostRequestBody {
-    @NotEmpty (message = "O campo descricao é obrigatório.")
+    @NotEmpty (message = "O campo descricao é obrigatório")
     private String descricao;
-    @NotEmpty (message = "O campo valor é obrigatório.")
+    @Min(value = 1,message = "O campo valor é obrigatório")
     private double valor;
     private int duracao;
 }
