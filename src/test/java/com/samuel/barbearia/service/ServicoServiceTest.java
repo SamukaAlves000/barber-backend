e2dc1c7e497721de9d4a7267e91337c96e5cfb96
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2021-01-28 12:19:20 -0300

    Ajustando teste e adicionando arquivo para definir versao java heroku

--- src/test/java/com/samuel/barbearia/service/ServicoServiceTest.java
+++ src/test/java/com/samuel/barbearia/service/ServicoServiceTest.java
@@ -28,98 +28,98 @@ class ServicoServiceTest {
     @Mock
     private ServicoRepository servicoRepositoryMock;
 
-//    @BeforeEach
-//    void setUp(){
-//
-//        BDDMockito.when(this.servicoRepositoryMock.findAll())
-//                .thenReturn(List.of(ServicoCreator.createServicoValid()));
-//
-//        BDDMockito.when(this.servicoRepositoryMock.findById(ArgumentMatchers.anyLong()))
-//                .thenReturn(Optional.of(ServicoCreator.createServicoValid()));
-//
-//        BDDMockito.when(this.servicoRepositoryMock.findAllByDescricao(ArgumentMatchers.anyString()))
-//                .thenReturn(List.of(ServicoCreator.createServicoValid()));
-//
-//        BDDMockito.when(this.servicoRepositoryMock.save(ArgumentMatchers.any(Servico.class)))
-//                .thenReturn(ServicoCreator.createServicoValid());
-//
-//        BDDMockito.doNothing().when(servicoRepositoryMock).delete(ArgumentMatchers.any(Servico.class));
-//    }
-//
-//
-//
-//    @Test
-//    @DisplayName("ListAll returns list of servicos when sucessful")
-//    void listAll_Returns_ListOfServicos_When_Successful(){
-//
-//        String expectDescricao = ServicoCreator.createServicoValid().getDescricao();
-//        List<Servico> servicoList = servicoService.findAll();
-//
-//        Assertions.assertThat(servicoList).isNotNull();
-//
-//        Assertions.assertThat(servicoList).isNotEmpty()
-//                .hasSize(1);
-//
-//        Assertions.assertThat(servicoList.get(0).getDescricao()).isEqualTo(expectDescricao);
-//    }
-//
-//    @Test
-//    @DisplayName("findById returns servico when sucessful")
-//    void findById_Returns_Servico_When_Successful(){
-//
-//        String expectDescricao = ServicoCreator.createServicoValid().getDescricao();
-//        Servico servico = servicoService.findById(1L);
-//
-//        Assertions.assertThat(servico).isNotNull();
-//
-//        Assertions.assertThat(servico.getDescricao()).isEqualTo(expectDescricao);
-//    }
-//
-//    @Test
-//    @DisplayName("ListAllByDescricao returns list of servicos when sucessful")
-//    void listAllByDescricao_Returns_ListOfServicos_When_Successful(){
-//
-//        List<Servico> servicoList = servicoService.findAllByDescricao("Corte Simples");
-//
-//        Assertions.assertThat(servicoList).isNotNull();
-//
-//        Assertions.assertThat(servicoList).isNotEmpty()
-//                .hasSize(1);
-//
-//        Assertions.assertThat(servicoList.get(0).getDescricao()).isEqualTo("Corte Simples");
-//    }
-//
-//
-//    @Test
-//    @DisplayName("save returns servico when sucessful")
-//    void save_Returns_Servico_When_Successful() {
-//
-//        Servico servico = ServicoCreator.createServicoValid();
-//        ServicoPostRequestBody servicoPostRequestBody = ServicoPostRequestBodyMapper.toServicoPostRequestBody(servico);
-//        Servico servicoSaved = servicoService.save(servicoPostRequestBody);
-//        Assertions.assertThat(servicoSaved).isNotNull().isEqualTo(servico);
-//
-//    }
-//
-//
-//    @Test
-//    @DisplayName("replace updates returns servico when sucessful")
-//    void save_Updates_Servico_When_Successful() {
-//
-//        Servico servico = ServicoCreator.createServicoValid();
-//        Assertions.assertThatCode(()-> servicoService.replace(ServicoPutRequestBodyMapper.toServicoPutRequestBody(servico)))
-//                .doesNotThrowAnyException();
-//
-//
-//    }
-//
-//    @Test
-//    @DisplayName("delete removes returns servico when sucessful")
-//    void delete_Removes_Servico_When_Successful() {
-//
-//        Servico servico = ServicoCreator.createServicoValid();
-//        Assertions.assertThatCode(()-> servicoService.delete(1L))
-//                .doesNotThrowAnyException();
-//    }
+    @BeforeEach
+    void setUp(){
+
+        BDDMockito.when(this.servicoRepositoryMock.findAll())
+                .thenReturn(List.of(ServicoCreator.createServicoValid()));
+
+        BDDMockito.when(this.servicoRepositoryMock.findById(ArgumentMatchers.anyLong()))
+                .thenReturn(Optional.of(ServicoCreator.createServicoValid()));
+
+        BDDMockito.when(this.servicoRepositoryMock.findAllByDescricao(ArgumentMatchers.anyString()))
+                .thenReturn(List.of(ServicoCreator.createServicoValid()));
+
+        BDDMockito.when(this.servicoRepositoryMock.save(ArgumentMatchers.any(Servico.class)))
+                .thenReturn(ServicoCreator.createServicoValid());
+
+        BDDMockito.doNothing().when(servicoRepositoryMock).delete(ArgumentMatchers.any(Servico.class));
+    }
+
+
+
+    @Test
+    @DisplayName("ListAll returns list of servicos when sucessful")
+    void listAll_Returns_ListOfServicos_When_Successful(){
+
+        String expectDescricao = ServicoCreator.createServicoValid().getDescricao();
+        List<Servico> servicoList = servicoService.findAll();
+
+        Assertions.assertThat(servicoList).isNotNull();
+
+        Assertions.assertThat(servicoList).isNotEmpty()
+                .hasSize(1);
+
+        Assertions.assertThat(servicoList.get(0).getDescricao()).isEqualTo(expectDescricao);
+    }
+
+    @Test
+    @DisplayName("findById returns servico when sucessful")
+    void findById_Returns_Servico_When_Successful(){
+
+        String expectDescricao = ServicoCreator.createServicoValid().getDescricao();
+        Servico servico = servicoService.findById(1L);
+
+        Assertions.assertThat(servico).isNotNull();
+
+        Assertions.assertThat(servico.getDescricao()).isEqualTo(expectDescricao);
+    }
+
+    @Test
+    @DisplayName("ListAllByDescricao returns list of servicos when sucessful")
+    void listAllByDescricao_Returns_ListOfServicos_When_Successful(){
+
+        List<Servico> servicoList = servicoService.findAllByDescricao("Corte Simples");
+
+        Assertions.assertThat(servicoList).isNotNull();
+
+        Assertions.assertThat(servicoList).isNotEmpty()
+                .hasSize(1);
+
+        Assertions.assertThat(servicoList.get(0).getDescricao()).isEqualTo("Corte Simples");
+    }
+
+
+    @Test
+    @DisplayName("save returns servico when sucessful")
+    void save_Returns_Servico_When_Successful() {
+
+        Servico servico = ServicoCreator.createServicoValid();
+        ServicoPostRequestBody servicoPostRequestBody = ServicoPostRequestBodyMapper.toServicoPostRequestBody(servico);
+        Servico servicoSaved = servicoService.save(servicoPostRequestBody);
+        Assertions.assertThat(servicoSaved).isNotNull().isEqualTo(servico);
+
+    }
+
+
+    @Test
+    @DisplayName("replace updates returns servico when sucessful")
+    void save_Updates_Servico_When_Successful() {
+
+        Servico servico = ServicoCreator.createServicoValid();
+        Assertions.assertThatCode(()-> servicoService.replace(ServicoPutRequestBodyMapper.toServicoPutRequestBody(servico)))
+                .doesNotThrowAnyException();
+
+
+    }
+
+    @Test
+    @DisplayName("delete removes returns servico when sucessful")
+    void delete_Removes_Servico_When_Successful() {
+
+        Servico servico = ServicoCreator.createServicoValid();
+        Assertions.assertThatCode(()-> servicoService.delete(1L))
+                .doesNotThrowAnyException();
+    }
 
 }
