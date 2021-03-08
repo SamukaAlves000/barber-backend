ec98adacd172b56788e0e5bd5502b9fd31d3f183
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2021-03-08 11:35:47 -0300

    Ajustando contexto dos teste

--- src/test/java/com/samuel/barbearia/BarbeariaApplicationTests.java
+++ src/test/java/com/samuel/barbearia/BarbeariaApplicationTests.java
@@ -3,11 +3,11 @@ package com.samuel.barbearia;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BarbeariaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
